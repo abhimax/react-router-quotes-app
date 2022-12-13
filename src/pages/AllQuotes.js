f467a38bdba7b13ceda8946aeb644db9c9d61ae5
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-12-13 06:56:57 +0530

    feat: add quote list and add quote form

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,4 +1,22 @@
+import QuoteList from '../components/quotes/QuoteList';
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Abhiman Ranaweera",
+    text: "Learning React is Fun!",
+  },
+  {
+    id: "q2",
+    author: "Lionel Messi",
+    text: "Learning TypeScript is Fun!",
+  },
+  {
+    id: "q3",
+    author: "Cristiano Ronaldo",
+    text: "Learning NextJS  is Fun!",
+  },
+];
 const AllQuotes = () => {
-    return <h1>All Quote Page</h1>
-}
+  return <QuoteList quotes={DUMMY_QUOTES}/>;
+};
 export default AllQuotes;
