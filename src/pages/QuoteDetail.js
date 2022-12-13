4bc506a8aae30eeb33ccea81b20284358198c724
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-12-13 21:00:49 +0530

    feat: quote detail page logic

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,10 +1,32 @@
 import { useParams, Route } from "react-router-dom";
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+const DUMMY_QUOTES = [
+    {
+      id: "q1",
+      author: "Abhiman Ranaweera",
+      text: "Learning React is Fun!",
+    },
+    {
+      id: "q2",
+      author: "Lionel Messi",
+      text: "Learning TypeScript is Fun!",
+    },
+    {
+      id: "q3",
+      author: "Cristiano Ronaldo",
+      text: "Learning NextJS  is Fun!",
+    },
+  ];
 const QuoteDetail = () => {
     const params = useParams();
+    const quote = DUMMY_QUOTES.find( qty => qty.id === params.quoteId);
+    if(!quote){
+        return <p>No Quote is found!</p>;
+
+    }
     return <>
-    <h1>Quote Detail Page</h1>
-    <p>{params.quoteId}</p>
+    <HighlightedQuote text={quote.text} author={quote.author}/>
     <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments/>
     </Route>
