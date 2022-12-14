eb76bc498a1d6bc814493de28d286a9f44981876
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-12-15 00:07:25 +0530

    feat: quote sorting feature

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -2,12 +2,36 @@ import { Fragment } from 'react';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
+import { useHistory, useLocation } from 'react-router-dom';
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const isSortAscending = queryParams.get('sort') === 'asc';
+
+  const sortedQuotes = sortQuotes(props.quotes,isSortAscending);
+
+  const changeSortHandler = () =>{
+    history.push(`./quotes?sort=${isSortAscending ? 'des':'asc'}`);
+  }
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortHandler}>Sort {isSortAscending ? 'Descending':'Ascending'}</button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
