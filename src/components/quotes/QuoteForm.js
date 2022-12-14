107c68e93ebca0b3914c8312c320a23715171210
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-12-14 07:58:06 +0530

    feat: prompt to handle form data when leave page

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,4 +1,5 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { Prompt } from 'react-router-dom';
 
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
@@ -7,6 +8,7 @@ import classes from './QuoteForm.module.css';
 const QuoteForm = (props) => {
   const authorInputRef = useRef();
   const textInputRef = useRef();
+  const [isEntered,setEntered] = useState(false);
 
   function submitFormHandler(event) {
     event.preventDefault();
@@ -19,9 +21,18 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
+  const formFocusHandler = () => {
+    setEntered(true);
+  };
+  const finishEnteringHandler = () => {
+    setEntered(false);
+  }
+
   return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
+    <>
+    <Prompt when={isEntered} message={()=>'Are you sure you want to leave? Your entered data will be lost!'}/>
+      <Card>
+      <form className={classes.form} onSubmit={submitFormHandler} onFocus={formFocusHandler}>
         {props.isLoading && (
           <div className={classes.loading}>
             <LoadingSpinner />
@@ -37,10 +48,11 @@ const QuoteForm = (props) => {
           <textarea id='text' rows='5' ref={textInputRef}></textarea>
         </div>
         <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
+          <button className='btn' onClick={finishEnteringHandler}>Add Quote</button>
         </div>
       </form>
     </Card>
+    </>
   );
 };
 
