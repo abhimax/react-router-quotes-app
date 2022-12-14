211244f6b570734fa1bc0f4a00a818caa2a79fe7
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-12-14 06:53:54 +0530

    feat: Implementing Programmatic Navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,7 +1,10 @@
+import { useHistory } from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm';
 const NewQuotes = () => {
+    const history = useHistory();
     const addQuoteHandler = quoteData => {
         console.log(quoteData);
+        history.push('./quotes');
     };
     return <QuoteForm onAddQuote={addQuoteHandler}/>
 }
