5c41711104e68088d0e79aad1dcb0993f4d5a123
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-12-16 04:20:25 +0530

    feat: use nested routes to get comments content

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,6 +1,7 @@
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
 const DUMMY_QUOTES = [
     {
       id: "q1",
@@ -27,6 +28,13 @@ const QuoteDetail = () => {
     }
     return <>
     <HighlightedQuote text={quote.text} author={quote.author}/>
+    
+    <Route path={`/quotes/${params.quoteId}`} exact>
+      <div className='centered'>
+        <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>Load Comments</Link>
+      </div>
+    </Route>
+
     <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments/>
     </Route>
