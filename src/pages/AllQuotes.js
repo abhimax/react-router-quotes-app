688936e14ba5051577f2c7b185a5907e6a7190a2
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-12-25 00:27:28 +0530

    feat: remove dummy quotes and fetch from server

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,22 +1,32 @@
+import { useEffect } from 'react';
 import QuoteList from '../components/quotes/QuoteList';
-const DUMMY_QUOTES = [
-  {
-    id: "q1",
-    author: "Abhiman Ranaweera",
-    text: "Learning React is Fun!",
-  },
-  {
-    id: "q2",
-    author: "Lionel Messi",
-    text: "Learning TypeScript is Fun!",
-  },
-  {
-    id: "q3",
-    author: "Cristiano Ronaldo",
-    text: "Learning NextJS  is Fun!",
-  },
-];
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
+
 const AllQuotes = () => {
-  return <QuoteList quotes={DUMMY_QUOTES}/>;
+  const {sendRequest, status, data: loadedQuote, error} = useHttp(getAllQuotes, true);
+  useEffect(() => {
+    sendRequest();
+  },[sendRequest]);
+
+  if(status === 'pending'){
+    return <div className='centered'>
+      <LoadingSpinner/>
+    </div>
+  }
+
+  if(error){
+    return <p className='centered focused'>
+      { error } 
+    </p>
+  }
+
+  if(status === 'completed' && (!loadedQuote || loadedQuote.length === 0)){
+    return <NoQuotesFound/>
+  }
+
+  return <QuoteList quotes={loadedQuote}/>;
 };
 export default AllQuotes;
