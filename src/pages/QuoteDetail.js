8d0cab3c05a8caff6d4441e00cf17a024a5e7e3b
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-12-25 01:23:06 +0530

    feat: remove dummy quote data and fetch data

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,34 +1,35 @@
+import { useEffect } from "react";
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
-const DUMMY_QUOTES = [
-    {
-      id: "q1",
-      author: "Abhiman Ranaweera",
-      text: "Learning React is Fun!",
-    },
-    {
-      id: "q2",
-      author: "Lionel Messi",
-      text: "Learning TypeScript is Fun!",
-    },
-    {
-      id: "q3",
-      author: "Cristiano Ronaldo",
-      text: "Learning NextJS  is Fun!",
-    },
-  ];
 const QuoteDetail = () => {
     const params = useParams();
+    const { quoteId } = params;
     const match = useRouteMatch();
-    const quote = DUMMY_QUOTES.find( qty => qty.id === params.quoteId);
-    if(!quote){
-        return <p>No Quote is found!</p>;
+    const {sendRequest, status, data: loadedQuote, error} = useHttp(getSingleQuote, true);
 
+    useEffect(() => {
+      sendRequest(quoteId);
+    },[sendRequest,quoteId]);
+
+    if(status === 'pending'){
+      return <div className="centered"><LoadingSpinner/></div>
     }
+
+    if(error){
+      return <p className="centered">{error}</p>
+    }
+
+    if(!loadedQuote.text){
+      return <p className="centered">No Quote Found!</p>
+    }
+
     return <>
-    <HighlightedQuote text={quote.text} author={quote.author}/>
+    <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author}/>
     
     <Route path={match.path} exact>
       <div className='centered'>
