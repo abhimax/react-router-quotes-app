6d3af8eb69f9bc9356c46c573231d0dac13c233c
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-12-27 01:00:49 +0530

    feat: add lazy loading for add new quote

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
+import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import AllQuotes from './pages/AllQuotes';
 import QuoteDetail from './pages/QuoteDetail';
-import NewQuote from './pages/NewQuote';
 import Layout from "./components/layout/Layout";
 import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote = React.lazy(()=> import('./pages/NewQuote'));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path={"/"} exact>
-          <Redirect to={"/quotes"} />
-        </Route>
+      <Suspense fallback={<div className="centered"><LoadingSpinner/></div>}>
+        <Switch>
+          <Route path={"/"} exact>
+            <Redirect to={"/quotes"} />
+          </Route>
 
-        <Route path={"/quotes"} exact>
-          <AllQuotes />
-        </Route>
+          <Route path={"/quotes"} exact>
+            <AllQuotes />
+          </Route>
 
-        <Route path={"/quotes/:quoteId"}>
-          <QuoteDetail />
-        </Route>
+          <Route path={"/quotes/:quoteId"}>
+            <QuoteDetail />
+          </Route>
 
-        <Route path={"/new-quote"}>
-          <NewQuote />
-        </Route>
+          <Route path={"/new-quote"}>
+            <NewQuote />
+          </Route>
 
-        <Route path={"*"}>
-          <NotFound />
-        </Route>
-      </Switch>
+          <Route path={"*"}>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
